b8f01c6687e6aa0287d66c75d714eb660e1e76c4
felipenchagas <felipe@empresarialweb.com.br>
2024-10-15 17:01:43 -0300

    d

--- processa_formulario-2-db.php
+++ processa_formulario-2-db.php
@@ -29,9 +29,9 @@ $conexao1 = new mysqli($servidor1, $usuario1, $senha1, $banco1);
 
 // Conectar ao segundo banco de dados (Locaweb)
 $servidor2 = "localhost";
-$usuario2 = "primeiro_estrusc";
+$usuario2 = "quarto_estrusc";
 $senha2 = "uRXA1r9Z7pv~Cw3"; // **ATENÇÃO:** Alterar imediatamente
-$banco2 = "primeiro_estruturasc";
+$banco2 = "quarto_estruturasc";
 $conexao2 = new mysqli($servidor2, $usuario2, $senha2, $banco2);
 
 // Verifica se a conexão foi bem-sucedida com ambos os bancos
